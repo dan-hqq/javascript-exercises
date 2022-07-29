d5670c416fc27a231baf7164e6aa2111e474a010
Dani <octaramadhanirinantoro@gmail.com>
2022-07-29 22:59:57 +0700

    Just do the README.md instructions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...element) {
+    for(const elm of element){
+        for(let i = 0; i < array.length; i++){
+            if(elm === array[i]){
+                array.splice(i, 1);
+                break;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
