b65373e444d445af556a4685a6da6d376828d34c
Dani <octaramadhanirinantoro@gmail.com>
2022-07-29 23:16:20 +0700

    Just do the README.md instructions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)){
+        return "ERROR";
+    }
+    if(+(num1) !== num1 || +(num2) !== num2){
+        return "ERROR";
+    }
+    if(num1 < 0 || num2 < 0){
+        return "ERROR"
+    }
+    if(num1 > num2){
+        return ((num1 - num2 + 1) / 2) * (2 * num2 + num1 - num2);
+    }
+    else if(num1 < num2){
+        return ((num2 - num1 + 1) / 2) * (2 * num1 + num2 - num1);
+    }
+    return num1;
 };
 
 // Do not edit below this line
